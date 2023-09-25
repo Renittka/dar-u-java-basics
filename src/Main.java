46408e6820832af35272f274f1320a3a3e969c2c
Anna Ogorodova <aogorodova@dar.io>
2023-09-25 17:36:11 +0200

    Added hello world

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("привет, мир!");
+        System.out.println("1 + 5 = " + (1 + 5));
+        System.out.println("Java");
+        System.out.print(". Без перехода на новую строку");
+        System.out.print(" 1");
+        System.out.print(" 2");
+        System.out.print(" 3");
+
+
+        // Однострочный комментарий
+
+        /*
+        1
+        2
+        3
+        4
+
+         */
+    }
+
+}
