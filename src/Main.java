22ef8c9b070458228d3904d5c3e82879d9e1df04
Anna Ogorodova <aogorodova@dar.io>
2023-10-02 16:57:35 +0200

    Added data types

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,26 @@
 public class Main {
+
     public static void main(String[] args) {
 
-        short a = 20;
-        short b = -20;
-        System.out.println(a + b);
-        System.out.println(a - b);
-        a = 40;
-        System.out.println(a);
-        System.out.println(b);
+        // Narrowing
+        double d = 24.5;
+        int a = (int)d;
+        System.out.println("Result of narrowing: " + a);
 
-        byte c = 10;
-        int d = -20;
-        int f2 = 20;
-        long l = 20L;
+        // 24   --> 0.5
+        // 24.0
+        float f = a;
+        System.out.println("Result: " + f);
 
-        int result = d + f2;
-        System.out.println(d + "+" + f2 + "=" + result);
-        System.out.println("Long: " + l);
+        // Widening
+        byte b = 7;
+        int i = b;
+        System.out.println("Result of widening: " + i);
 
-        double g = 2.5;
-        float f = 34.8f;
-
-        System.out.println(g);
-        System.out.println(f);
-
-        // System.out.println(letterA);
-        char letterA = 65, letterB = 66;
-
-        char myNewLetterD = 68;
-
-        System.out.println(letterA);
-
-        boolean flag = true;
-        System.out.println(flag);
-        flag = false;
-
-        boolean flag1 = false;
-
-        System.out.println(flag);
-
-        String str = null;
-        System.out.println(str);
-
-        System.out.println(str);
-
-        str = "Я изучаю java";
-        System.out.println(str);
-
-        int a1 = 5, b1 = 6;
-        System.out.println(a1 + b1);
-
-        int x, y, z;
-        x = y = z = 50;
-        System.out.println(x + y + z);
-
-
-        String name = "Maria";
-        System.out.println("Hello, " + name + "! " + 123 + 123);
-
-        final double PI = 3.14;
-
-        int myNumber = 5;
-        int mynumber = 10;
-
-        /*
-        System.out.println("привет, мир!");
-        System.out.println("1 + 5 = " + (1 + 5));
-        System.out.println("Java");
-        System.out.print(". Без перехода на новую строку");
-        System.out.print(" 1");
-        System.out.print(" 2");
-        System.out.print(" 3");
-
-        // Однострочный комментарий
-        */
-
-        doSomething();
-
+        float f1 = 24.5f;
+        double d1 = f1;
+        System.out.println("Result of  float-double: " + d1);
     }
 
-    static void doSomething() {
-        int a = 10;
-        System.out.println(a);
-    }
 
 }
