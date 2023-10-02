752172c786247eb43d2059577c9807f2d05c8818
Anna Ogorodova <aogorodova@dar.io>
2023-10-02 17:34:48 +0200

    Changed main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         // Narrowing
         double d = 24.5;
-        int a = (int)d;
+        int a = (int) d;
         System.out.println("Result of narrowing: " + a);
 
         // 24   --> 0.5
@@ -20,6 +20,44 @@ public class Main {
         float f1 = 24.5f;
         double d1 = f1;
         System.out.println("Result of  float-double: " + d1);
+
+        System.out.println(20 / 2);
+
+
+        int result = 20 / 2;
+        int doubleResult = (int) (20 / 2.7);
+        double doubleRes = 20 / 2.7;
+        System.out.println(doubleResult);
+        System.out.println(doubleRes);
+
+
+        a = 20;
+        System.out.println("Original: " + a);
+        System.out.println("Prefix increment: " + (++a));
+        System.out.println("Postfix increment: " + (a++));
+        System.out.println("Final: " + a);
+
+        System.out.println("------------");
+        a = 20;
+        System.out.println("Original: " + a);
+        System.out.println("Prefix decrement: " + (--a)); //1. a = a - 1 = 20 - 1; 2. Вывод
+        System.out.println("Postfix decrement: " + (a--)); // 1. Вывод 2. 19 - 1 = 18
+        System.out.println("Final: " + a);
+
+        int sum = a + ++b;
+        System.out.println(sum);
+
+        System.out.println("------------");
+
+        for(int i1 = 0; i1 < 5; i1++) {
+            System.out.println(i1);
+        }
+        System.out.println("------------");
+
+        for (int i1 = 5; i1 > 0; i1++) {
+            System.out.println(i1);
+        }
+
     }
 
 
